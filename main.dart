571d29b0236e51ee45a82835d7f041e14fc9eecf
Christian Machira <christianmachira8@gmail.com>
2024-03-07 15:50:23 +0300

    first commit

---
+++ main.dart
@@ -0,0 +1,34 @@
+//dart introduction codes
+void main() {
+  //dart data types
+  String name = "Christian";
+  int age = 23;
+  double height = 5.11;
+  bool isHandsome = true;
+  print(
+      "My name is $name I am $age years of age, I play basketball at the height of $height and it is $isHandsome that I am handsome");
+//creating lists
+  List<String> myList = [
+    "FERRARI",
+    "REDBULL",
+    "MERCEDES",
+    "MCLAREN",
+    "VISACASHAPPRB",
+    "ALFA ROMEO",
+    "HAAS",
+    "WILLIAMS",
+    "ASTON MARTIN"
+  ];
+  print(myList);
+  //creating maps
+  Map<String, int> myMap = {
+    "Charles Leclerc": 16,
+    "Max Verstappen": 1,
+    "Lewis Hamilton": 44,
+    "Lando Norris": 4,
+    "Yuki Tsunoda": 22,
+    "Kimi Raikkonen": 7,
+    "Nico Hulkenberg": 27,
+  };
+  print(myMap);
+}
